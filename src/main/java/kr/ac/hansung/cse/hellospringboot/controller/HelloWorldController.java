1f92c177536bb7b5026a51b73022b94524c44fe0
ul4382 <ul4382@hansung.ac.kr>
2021-04-18 12:03:15 +0900

    add sayhi

--- src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
     public String sayHello(){
         return "index";
     }
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "index";
+    }
 }
